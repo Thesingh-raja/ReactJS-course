f41093a5026e0e98f82fb187e4ace728660d8052
Thesingh raja <thesingh.raja@codem.com>
2022-02-23 12:08:42 +0530

    task completed

---
+++ Section-11/src/App.js
@@ -0,0 +1,29 @@
+import {useState } from 'react';
+import CartProvider from './store/cartProvider'
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+
+function App() {
+  const[cartIsShown,setcartIsShown]= useState(false)
+
+  const showCartHandler=()=>
+  {
+    setcartIsShown(true)
+  }
+  const hideCartHandler=()=>
+  {
+    setcartIsShown(false)
+  }
+  return (
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
+  );
+}
+
+export default App;
